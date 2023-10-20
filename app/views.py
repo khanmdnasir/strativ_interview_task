56b7215669f0453765d17c164a844d5197ae90e0
Md. Nasir Khan <49349351+khanmdnasir@users.noreply.github.com>
2023-10-20 20:46:49 +0600

    api endpoint complete for getting coolest 10 district list

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from rest_framework import status
-from .models import *
-from .serializers import *
+from .models import District
+from .serializers import DistrictSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
 
+
 class Coolest10DistricsApi(APIView):
-    def get(self,request):        
-        pass
-        return Response({'success':True})
+    def get(self, request):
+        districts = District.objects.all().order_by('avg_temp')[:10]
+        serializer = DistrictSerializer(districts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
